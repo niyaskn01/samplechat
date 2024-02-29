113a47a07b1fd322943e2d920868ceec069e4627
niyas <niyaskn01@gmail.com>
2024-02-29 15:52:38 +0530

    URL changed

--- client/src/axios/axios.js
+++ client/src/axios/axios.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 export const axiosInstance=axios.create({
-  baseURL:'http://localhost:9000'
+  // baseURL:'http://localhost:9000'
+  baseURL:'https://samplechatserver.vercel.app'
 })
 
-export const baseURL='http://localhost:9000'
+// export const baseURL='http://localhost:9000'
+export const baseURL='https://samplechatserver.vercel.app'
